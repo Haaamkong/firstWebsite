895e16f62ebd35902f04b7e38c0e2e20690c2265
hamkong <0514jhm@gmail.com>
2022-07-28 20:02:33 +0900

    반복문 연습, 코딩테스트 연습

---
+++ solution.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  var i = 0;
+  var sum = 0;
+  while(i < arr.length){
+    sum = sum + arr[i++];
+    i = i + 1;
+  }
+  return sum / arr.length;
+}
+
+console.log(solution([1,2,3,4]));
